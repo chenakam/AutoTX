153660327bdd9ce7dd96f4c24684a46f5889d228
成才 '中© <chenai.nakam@gmail.com>
2018-07-12 23:18:06 +0800

    upd |> 临时同步。

--- src/main/scala/hobby/chenai/nakam/autotx/AutoTx.scala
+++ src/main/scala/hobby/chenai/nakam/autotx/AutoTx.scala
@@ -16,10 +16,13 @@
 
 package hobby.chenai.nakam.autotx
 
-import hobby.chenai.nakam.autotx.core.coin.{AbsCashGroup, AbsTokenGroup}
-import hobby.chenai.nakam.autotx.core.coin.CnyGroup.wrapCnyNum
+import hobby.chenai.nakam.autotx.core.coin.{AbsCashGroup, AbsTokenGroup, CnyGroup}
 import hobby.chenai.nakam.autotx.core.coin.EthGroup._
-
+import hobby.chenai.nakam.autotx.task.{kces, CounterParty, RealTimePriceScheduler}
+import hobby.wei.c.reflow
+import hobby.wei.c.reflow.{Out, Poster, Reflow}
+import hobby.wei.c.reflow.implicits._
+import hobby.wei.c.reflow.Feedback.Progress.Policy
 import scala.language.postfixOps
 
 /**
@@ -27,69 +30,93 @@ import scala.language.postfixOps
   * @version 1.0, 10/07/2018
   */
 object AutoTx {
+  // TODO: 注意在 Android 平台上需要修改。
+  // Reflow.setThreadResetor(new ThreadResetor {})
+  // Reflow.setLogger()
+  Reflow.setDebugMode(true)
 
-  // TODO: 整体流程
+  implicit lazy val policy: Policy = Policy.Depth(3) -> Policy.Fluent -> Policy.Interval(600)
+  implicit lazy val poster: Poster = null
 
-  // 如何触发。定时任务？
-  load交易对And计价规则() // 并行。
+  object SupportedExchange extends Enumeration {
+    type Tpe = Exchange
+    private[SupportedExchange] case class Exchange(name: String) extends Val
+    val HuoBi = Exchange("HuoBi")
+  }
+
+  def startOnAll(): Unit = SupportedExchange.values.foreach(_ => startOn(_))
+
+  def startOn(exchange: SupportedExchange.Tpe): Unit = {
+    Reflow.create(CounterParty(exchange))
+      .next(RealTimePriceScheduler())
+      .submit("启动器", none)
+      .start((kces.exchangeName, exchange.name), new reflow.Feedback.Adapter {
+        override def onComplete(out: Out): Unit = {
+          Reflow.create()
+        }
+      })
+  }
 
   // TODO: 在独立进程实时运算
   load市场实时数据()
-  分析优质币()
+  Analyzer4CNY.分析优质币()
 
-  def entryOrdersIntelligent(): Unit = {
-    val (是, 额度) = 是否继续投入资金
-    if (是) {
-      val 优质币种 = 筛选出指数符合用户设置(分析优质币())
-      val 买 = true
-      val 目标 = 具备最佳时机的(优质币种)
-      if (目标.nonEmpty) autoEntryOrdersOfBuying(买, 额度, 目标)
-      else {
-        // 等待事件触发调用：余额变动，用户设置更改，用户手动触发
+  object Analyzer4CNY extends Analyzer(CnyGroup)
+
+  abstract class Analyzer(protected val cash: AbsCashGroup) {
+    def entryOrdersIntelligent(): Unit = {
+      val (yes, mount) = 是否继续投入资金
+      if (yes) {
+        val 优质币种 = 筛选出指数符合用户设置(分析优质币())
+        val 目标 = 具备最佳时机的(优质币种)
+        if (目标.nonEmpty) autoEntryOrdersOfBuying(mount, 目标)
+        else {
+          // 等待事件触发调用：余额变动，用户设置更改，用户手动触发
+        }
+      } else {
+        // 等待事件触发调用：余额变动，用户设置更改，用户手动触发。
       }
-    } else {
-      // 等待事件触发调用：余额变动，用户设置更改，用户手动触发。
     }
-  }
 
-  // 具体的一个订单
-  def entryOrderOfBuying(buyOrSell: Boolean, money: AbsCashGroup#COIN, token: AbsTokenGroup): Unit = {
-    // TODO: http 请求参数，用户是否注册，apiKey/apiSecret。
-  }
-
-  def 具备最佳时机的(优质币: Seq[(AbsTokenGroup, Double)]): Seq[(AbsTokenGroup, Double /*建议购买比例*/ )] = {
-    // 分析舆论情况。牛市 or 熊市。
-    // 时机是什么？哪个最有暴涨的趋势（最小的代价可以拉起来）。
-    if (优质币.isEmpty) Nil else Nil
-  }
-
-  def autoEntryOrdersOfBuying(买or卖: Boolean, 金额: AbsCashGroup#COIN, 优质币种: Seq[(AbsTokenGroup, Double)]): Unit = {
-    优质币种.foreach { 币 =>
-      // TODO: 拆分成几单，需要计算最优解。
-      val each = 金额 / 10
-      (0 until 10).foreach { _ => entryOrderOfBuying(买or卖, each * 币._2, 币._1) }
+    // 具体的一个订单
+    def entryOrderOfBuying(money: AbsCashGroup#AbsCash, token: AbsTokenGroup): Unit = {
+      // TODO: http 请求参数，用户是否注册，apiKey/apiSecret。
     }
+
+    def 具备最佳时机的(优质币: Seq[(AbsTokenGroup, Double)]): Seq[(AbsTokenGroup, Double /*建议购买比例*/ )] = {
+      // 分析舆论情况。牛市 or 熊市。
+      // 时机是什么？哪个最有暴涨的趋势（最小的代价可以拉起来）。
+      if (优质币.isEmpty) Nil else Nil
+    }
+
+    def autoEntryOrdersOfBuying(金额: AbsCashGroup#AbsCash, 优质币种: Seq[(AbsTokenGroup, Double)]): Unit = {
+      优质币种.foreach { 币 =>
+        // TODO: 拆分成几单，需要计算最优解。
+        val each = 金额 / 10
+        (0 until 10).foreach { _ => entryOrderOfBuying(each * 币._2, 币._1) }
+      }
+    }
+
+    def 分析优质币(): Seq[(AbsTokenGroup, Double /*增长指数（综合评分）*/ )] = {
+      // 单位时间内（一天？一小时？）上涨概率、涨幅。需要把上述参数综合评估。
+      (ETH.group, 0.65) :: Nil
+    }
+
+    def 筛选出指数符合用户设置(优质币: Seq[(AbsTokenGroup, Double)]): Seq[(AbsTokenGroup, Double /*增长指数（综合评分）*/ )] = {
+      优质币
+    }
+
+    /** 是否继续投入资金。 */
+    def 是否继续投入资金: (Boolean, AbsCashGroup#AbsCash) = (可用现金额 >= 可挂单最低现金额, 可用现金额)
+
+    def 可挂单最低现金额: cash.COIN = ???
+
+    def 可用现金额: cash.COIN = 现金总余额 - 现金备用预留
+
+    def 现金总余额: cash.COIN = ???
+
+    def 现金备用预留: cash.COIN = ???
   }
-
-  def 分析优质币(): Seq[(AbsTokenGroup, Double /*增长指数（综合评分）*/ )] = {
-    // 单位时间内（一天？一小时？）上涨概率、涨幅。需要把上述参数综合评估。
-    (ETH.group, 0.65) :: Nil
-  }
-
-  def 筛选出指数符合用户设置(优质币: Seq[(AbsTokenGroup, Double)]): Seq[(AbsTokenGroup, Double /*增长指数（综合评分）*/ )] = {
-    优质币
-  }
-
-  def 是否继续投入资金: (Boolean, AbsCashGroup#COIN) = (可用现金额 >= 可挂单最低现金额, 可用现金额)
-
-  def 可挂单最低现金额: AbsCashGroup#COIN = 50 CNY
-
-  def 可用现金额: AbsCashGroup#COIN = 现金总余额 - 现金备用预留
-
-  def 现金总余额: AbsCashGroup#COIN = 10e6 CNY
-
-  def 现金备用预留: AbsCashGroup#COIN = 10e5 CNY
-
 
   def load交易对And计价规则(): Unit = {
     // TODO: 存入数据库，更新频率1天1次。可强制刷新。
@@ -97,9 +124,5 @@ object AutoTx {
 
   def load市场实时数据(): Unit = {
     // TODO: 根据限定频率（如5分钟600次调用）实时刷新数据。存库以供分析。
-  }
-
-  def loadData(): Unit = {
-    // TODO: 。
   }
 }
